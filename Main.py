bcda0944adbfa4d70f58782502a4333c355d63e9
GSKirox <65768236+GSKirox@users.noreply.github.com>
2025-12-20 10:51:56 +0100

    Fix presets

--- Main.py
+++ Main.py
@@ -164,9 +164,6 @@ def build_world_graphs(settings: Settings) -> list[World]:
         if world.settings.shuffle_dungeon_rewards in ('vanilla', 'reward'):
             world.fill_bosses()
 
-        if settings.empty_dungeons_mode == 'rewards':
-            world.set_empty_dungeon_rewards(settings.empty_dungeons_rewards)
-
     if settings.triforce_hunt:
         settings.distribution.configure_triforce_hunt(worlds)
 
